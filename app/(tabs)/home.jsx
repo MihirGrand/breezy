33623e287f052c67287286c7921ae6adb6202b12
Mihir <118926177+MihirGrand@users.noreply.github.com>
2024-06-09 17:34:40 +0530

    fetching data from tmdb now

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,112 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+
+  const popularUrl = "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  const nowPlayingUrl = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
+  const topRatedUrl = "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+  const upcomingUrl = "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1";
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.EXPO_PUBLIC_TMDB_SECRET}`,
+    },
+  };
+
+  useEffect(() => {
+    fetch(popularUrl, options)
+      .then((res) => res.json())
+      .then((json) => setPopular(json.results))
+      .catch((err) => console.error("error:" + err));
+    fetch(nowPlayingUrl, options)
+      .then((res) => res.json())
+      .then((json) => setNowPlaying(json.results))
+      .catch((err) => console.error("error:" + err));
+    fetch(topRatedUrl, options)
+      .then((res) => res.json())
+      .then((json) => setTopRated(json.results))
+      .catch((err) => console.error("error:" + err));
+    fetch(upcomingUrl, options)
+      .then((res) => res.json())
+      .then((json) => setUpcoming(json.results))
+      .catch((err) => console.error("error:" + err));
+  }, []);
   return (
     <View>
       <Text>Home</Text>
+      <ScrollView>
+        <ScrollView horizontal={true}>
+          <View className="flex-row gap-2">
+            {popular.map((movie) => {
+              return (
+                <View key={movie.id} className="w-20">
+                  <Image
+                    className="h-40 rounded-md"
+                    source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                    resizeMode="cover"
+                  />
+                  <Text className=" font-medium text-wrap text-center">{movie.title}</Text>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <ScrollView horizontal={true}>
+          <View className="flex-row gap-2">
+            {nowPlaying.map((movie) => {
+              return (
+                <View key={movie.id} className="w-20">
+                  <Image
+                    className="h-40 rounded-md"
+                    source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                    resizeMode="cover"
+                  />
+                  <Text className=" font-medium text-wrap text-center">{movie.title}</Text>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <ScrollView horizontal={true}>
+          <View className="flex-row gap-2">
+            {topRated.map((movie) => {
+              return (
+                <View key={movie.id} className="w-20">
+                  <Image
+                    className="h-40 rounded-md"
+                    source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                    resizeMode="cover"
+                  />
+                  <Text className=" font-medium text-wrap text-center">{movie.title}</Text>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+        <ScrollView horizontal={true}>
+          <View className="flex-row gap-2">
+            {upcoming.map((movie) => {
+              return (
+                <View key={movie.id} className="w-20">
+                  <Image
+                    className="h-40 rounded-md"
+                    source={{ uri: `https://image.tmdb.org/t/p/w500${movie.poster_path}` }}
+                    resizeMode="cover"
+                  />
+                  <Text className=" font-medium text-wrap text-center">{movie.title}</Text>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </ScrollView>
     </View>
   );
 };
--- app/index.jsx
+++ app/index.jsx
@@ -2,9 +2,12 @@ import { StatusBar } from "expo-status-bar";
 import { Text, View, TextInput, SafeAreaView, Button } from "react-native";
 import { Link, router } from "expo-router";
 
+import images from "../constants/images";
+
 export default function App() {
   return (
-    <SafeAreaView className="bg-bgdark h-full pt-10">
+    <SafeAreaView className="flex-1 gap-2 bg-bgdark h-full pt-10 items-center">
+      <images.Video_Streaming height={300} className="drop-shadow" />
       <Button title="Home" onPress={() => router.push("home")}>
         Home
       </Button>
